09bf9b50fc9a1e9300580af592551439f7764c2d
funfunDaniel <54621371+funfunDaniel@users.noreply.github.com>
2020-02-01 16:11:32 +0700

    change IP

--- src/GOBALS.js
+++ src/GOBALS.js
@@ -12,3 +12,11 @@ export default {
 	URL_UPLOAD: 'http://191.1.0.180:5001/upload-image/',
 	URL_DELETE: 'http://191.1.0.180:5001/delete-image/',
 }
+
+// *************Revel Soft************* */
+// export default {
+// 	URL: 'http://13.229.233.71:5001/',
+// 	URL_IMG: 'http://13.229.233.71:5001/',
+// 	URL_UPLOAD: 'http://13.229.233.71:5001/upload-image/',
+// 	URL_DELETE: 'http://13.229.233.71:5001/delete-image/',
+// }
