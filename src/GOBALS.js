1bad1c827a545640ea9e643051a2b9cc72870b69
funfunDaniel <54621371+funfunDaniel@users.noreply.github.com>
2020-02-01 16:12:14 +0700

    Revert "backend"
    This reverts commit 243abc005dc8a5e934cac8eb4e050dc9203cf72a.

--- src/GOBALS.js
+++ src/GOBALS.js
@@ -7,10 +7,10 @@
 // }
 // *************HOST************* */
 export default {
-	URL: 'http://191.1.0.180:5001/',
-	URL_IMG: 'http://191.1.0.180:5001/',
-	URL_UPLOAD: 'http://191.1.0.180:5001/upload-image/',
-	URL_DELETE: 'http://191.1.0.180:5001/delete-image/',
+	URL: 'http://13.229.233.71:5001/',
+	URL_IMG: 'http://13.229.233.71:5001/',
+	URL_UPLOAD: 'http://13.229.233.71:5001/upload-image/',
+	URL_DELETE: 'http://13.229.233.71:5001/delete-image/',
 }
 
 // *************Revel Soft************* */
